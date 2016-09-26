3fc7fe49753ebb9b85a1137a3f94f4066b0b203e
Kenneth Steele <kvsteele@berkeley.edu>
2016-09-25 17:59:03 -0700

    Text of topics and text set to design spec

--- app/assets/javascripts/components/program_page.es6.jsx
+++ app/assets/javascripts/components/program_page.es6.jsx
@@ -3,9 +3,17 @@ class ProgramPage extends React.Component {
   get styles() {
     return {
       bodycontent: {
-      	padding: '0 50px',
+      	padding: '0 70px',
         backgroundColor: StyleConstants.colors.white,
       },
+
+      topic: {
+      	color: StyleConstants.colors.orange,
+      },
+
+      textintopic: {
+      	color: StyleConstants.colors.grey,
+      }
     };
   }
 
@@ -15,13 +23,15 @@ class ProgramPage extends React.Component {
         <Header />
 
         <div style={this.styles.bodycontent}>
-	        <h1> How Forte Works </h1>
+	        <h1 style={this.styles.topic}> How Forte Works </h1>
 
-	        	<h3> We connect aspiring musicians with instructions to offer 
+	        	<h3 style={this.styles.textintopic}> 
+	        	We connect aspiring musicians with instructions to offer 
 	        	lessons at deeply discounted rates.
 	        	</h3>
 
-			        <p> Far too often, aspiring youth musicians are barred from pursuing their 
+			        <p style={this.styles.textintopic}> 
+			        Far too often, aspiring youth musicians are barred from pursuing their 
 			        musical passions due to their financial situation. Forte’s online platform 
 			        matches underprivileged youth with experienced musicians willing to provide 
 			        deeply discounted music lessons. Our passionate teachers give back to the 
@@ -31,26 +41,30 @@ class ProgramPage extends React.Component {
 			        45-minute session. 
 			        </p>
 
-			        <p> Prospective students and teachers apply for out program online with a 
+			        <p style={this.styles.textintopic}> 
+			        Prospective students and teachers apply for out program online with a 
 			        brief survey including personal details, instrument interest and mobility 
 			        preferences. Selected individuals are then invited to register for our 
 			        online platform where they can schedule weekly 45-minute lessons, provide 
 			        feedback and access our educational resources. 
 			        </p>
 
-			        <p> Prospective students and teachers are thoroughly vetted by our team 
+			        <p style={this.styles.textintopic}> 
+			        Prospective students and teachers are thoroughly vetted by our team 
 			        before being invited to use our platform. This includes detailed background 
 			        checks for all instructors and strict eligibility requirements for students 
 			        based on financial need. 
 			        </p>
 
-	        <h1> Our Teachers </h1>
+	        <h1 style={this.styles.topic}> Our Teachers </h1>
 
-	        	<h3> We are deeply rooted in the tradition of using music to serve our 
+	        	<h3 style={this.styles.textintopic}> 
+	        	We are deeply rooted in the tradition of using music to serve our 
 	        	community.
 	        	</h3>
 
-			        <p> Forte’s roots trace back to 2008, when our CEO formed a service 
+			        <p style={this.styles.textintopic}> 
+			        Forte’s roots trace back to 2008, when our CEO formed a service 
 			        organization at Homestead High School allowing student musicians to 
 			        collaborate via small ensembles. Ranging from duos to quintets, these 
 			        intimate groups inspired levels of student independence and creativity 
@@ -60,13 +74,16 @@ class ProgramPage extends React.Component {
 			        including HCR ManorCare, the Santa Clara Valley Blind Center, and the 
 			        J.W. House of Kaiser Permanente. 
 			        </p>
-	        <h1> Our Students </h1>
 
-	        	<h3> We are deeply rooted in the tradition of using music to serve our 
+	        <h1 style={this.styles.topic}> Our Students </h1>
+
+	        	<h3 style={this.styles.textintopic}> 
+	        	We are deeply rooted in the tradition of using music to serve our 
 	        	community.
 	        	</h3>
 
-			        <p> Forte’s roots trace back to 2008, when our CEO formed a service 
+			        <p style={this.styles.textintopic}> 
+			        Forte’s roots trace back to 2008, when our CEO formed a service 
 			        organization at Homestead High School allowing student musicians to 
 			        collaborate via small ensembles. Ranging from duos to quintets, these 
 			        intimate groups inspired levels of student independence and creativity 
