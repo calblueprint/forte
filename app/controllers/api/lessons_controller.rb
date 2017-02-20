dbf5a7282beb113324cb61c60825d23930aa7aa7
Eric Liang <liang.eric24@gmail.com>
2017-02-20 12:30:13 -0800

    lesson controller fix

--- app/controllers/api/lessons_controller.rb
+++ app/controllers/api/lessons_controller.rb
@@ -16,12 +16,12 @@ class Api::LessonsController < Api::BaseController
   def update
     lesson = Lesson.find params[:id]
     # TODO: If the update only changes price and not time, then we shouldnt' send a reschedule email, but a Lesson Price Changed email.
-    # if params[:is_paid]
-    #   # Send payment email
-    # else
-    #   # Send rescheduled lesson email
-    #   lesson.send_reschedule_emails
-    # end
+    if params[:is_paid]
+      # Send payment email
+    elif params[:start_time] and params[:end_time]
+      # Send rescheduled lesson email
+      lesson.send_reschedule_emails
+    end
 
     if lesson.update_attributes lesson_params
       render json: lesson
