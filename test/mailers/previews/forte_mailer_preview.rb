896615ea34a0f1adcbcaaefa6215aeb8552fa3c3
Eric Liang <liang.eric24@gmail.com>
2017-03-06 11:56:23 -0800

    remove mailer preview test file

--- test/mailers/previews/forte_mailer_preview.rb
+++
@@ -1,5 +0,0 @@
-class ForteMailerPreview < ActionMailer::Preview
-  def send_contact_email
-    ForteMailer.send_contact_email("Happy Birthday", "Eric", "Liang", "test@email.com", "YAAAAy")
-  end
-end
